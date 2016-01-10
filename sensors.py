0626f0a2a1b3e344bf60c653148f7bdeef3e25d8
dalcomkenya <dalcomke@gmail.com>
2016-01-11 01:30:57 +0300

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -1,62 +1,85 @@
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
-
-
 #import modules
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD 
 import time
 
-    import RPi.GPIO as GPIO           # import RPi.GPIO module  
-     
-      
-     
-    GPIO.output(port_or_pin, 1)       # set an output port/pin value to 1/HIGH/True  
-    GPIO.output(port_or_pin, 0)       # set an output port/pin value to 0/LOW/False  
-    i = GPIO.input(port_or_pin)       # read status of pin/port and assign to variable i  
+#variables
+#
+#sensors
+v_s = 
+m_s = 
+t_s = 
+#
+#actuators
+stb = 
+srn = 
 
 #initialize sensors
 #
 #vibration sensors
-GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
+GPIO.setup(v_s, GPIO.IN)  # set a port/pin as an input
 #
 #motion sensors
-GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
+GPIO.setup(m_s, GPIO.IN)  # set a port/pin as an input
 #
 #trap switches
-GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
+GPIO.setup(t_s, GPIO.IN)  # set a port/pin as an input
 #
 #
 
 #initialize actuators
 #
 #siren
-GPIO.setup(port_or_pin, GPIO.OUT) # set a port/pin as an output  
+GPIO.setup(srn, GPIO.OUT) # set a port/pin as an output  
 #
 #strobes
-GPIO.setup(port_or_pin, GPIO.OUT) # set a port/pin as an output  
+GPIO.setup(stb, GPIO.OUT) # set a port/pin as an output  
 
 
 #initialize functions
 #
+#activate actuators
+def actuatorsOn:
+    GPIO.output(stb, 1) and GPIO.output(srn, 1)
+#
+#deactivate actuators
+def actuatorsOn:
+    GPIO.output(stb, 0) and GPIO.output(srn, 0)
+#
 #camera capture
+def camera:
 #
 #send image over
+def sendImages:
 
 ##loop
 #
-#if v.s and m.s and t.s are active, activate 
-#
-#if v.s and m.s are active
-#
-#if v.s and t.s are active
-#
-#if m.s and t.s are active
+try:
+    while True:
+        #if v.s and m.s and t.s are active, activate 
+        if GPIO.input(v_s) and GPIO.input(m_s) and GPIO.input(t_s): # if port 25 == 1 
+            actuatorsOn
+            camera
+            sendImages
+        #
+        #if v.s and m.s are active
+        if GPIO.input(v_s) and GPIO.input(m_s): # if port 25 == 1 
+            actuatorsOn
+            camera
+            sendImages
+        #
+        #if v.s and t.s are active
+        if GPIO.input(v_s) and GPIO.input(t_s): # if port 25 == 1 
+            actuatorsOn
+            camera
+            sendImages
+        #
+        #if m.s and t.s are active
+        if GPIO.input(m_s) and GPIO.input(t_s): # if port 25 == 1 
+            actuatorsOn
+            camera
+            sendImages 
+            
+finally:                   # this block will run no matter how the try block exits  
+    GPIO.cleanup()         # clean up after yourself  
+    
