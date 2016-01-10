0167be02ad0848932b4ac5cea8794324748c4d32
dalcomkenya <dalcomke@gmail.com>
2016-01-11 00:28:22 +0300

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -1,5 +1,4 @@
-import RPi.GPIO as GPIO
-import time
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -12,22 +11,39 @@ while True:
 
 
 #import modules
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)            # choose BCM or BOARD 
+import time
+
+    import RPi.GPIO as GPIO           # import RPi.GPIO module  
+     
+      
+     
+    GPIO.output(port_or_pin, 1)       # set an output port/pin value to 1/HIGH/True  
+    GPIO.output(port_or_pin, 0)       # set an output port/pin value to 0/LOW/False  
+    i = GPIO.input(port_or_pin)       # read status of pin/port and assign to variable i  
 
 #initialize sensors
 #
 #vibration sensors
+GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
 #
 #motion sensors
+GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
 #
 #trap switches
+GPIO.setup(port_or_pin, GPIO.IN)  # set a port/pin as an input
 #
 #
 
 #initialize actuators
 #
 #siren
+GPIO.setup(port_or_pin, GPIO.OUT) # set a port/pin as an output  
 #
 #strobes
+GPIO.setup(port_or_pin, GPIO.OUT) # set a port/pin as an output  
+
 
 #initialize functions
 #
